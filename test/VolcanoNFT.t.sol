cd45a7d213e704de7aa5810159cb65cc4871bc69
ericbadiere <eric.badiere@coinme.com>
2022-11-05 21:56:13 -0600

    Working.

--- test/VolcanoNFT.t.sol
+++ test/VolcanoNFT.t.sol
@@ -20,38 +20,33 @@ contract VolcanoNFTTest is Test {
     VolcanoNFT public volcanoNFT;
 
     function setUp()public {
-        // utils = new Utils();
-        // users = utils.createUsers(3);
-        // alice = users[0];
-        // vm.label(alice, "Alice");
-        // bob = users[1];
-        // vm.label(bob, "Bob");
-        // dave = users[2];
+        utils = new Utils();
+        users = utils.createUsers(3);
+        alice = users[0];
+        vm.label(alice, "Alice");
+        bob = users[1];
+        vm.label(bob, "Bob");
+        dave = users[2];
 
-        // vm.prank(address(dave));
+        vm.prank(address(dave));
         volcanoNFT = new VolcanoNFT();
     }
 
     function testMint() public {
-
-        // console.log("%s: %d", "Dave", address(dave));
-
-        // emit log_address(volcanoNFT.owner());
-
-        // vm.prank(address(dave));
-
-        uint256 itemId = volcanoNFT.mintNFT{value: 0.01 ether}(address(1), "https://pixabay.com/photos/new-zealand-volcano-crater-3018634/");
+        vm.prank(address(dave));
+        uint256 itemId = volcanoNFT.mintNFT{value: 0.01 ether}(address(alice), "https://pixabay.com/photos/new-zealand-volcano-crater-3018634/");
         assert(itemId > 0);
-        assertEq(volcanoNFT.balanceOf(address(1)), 1);
+        assertEq(volcanoNFT.balanceOf(address(alice)), 1);
     }
 
     function testTransfer() public {
-        // vm.prank(address(dave));
-        uint256 itemId = volcanoNFT.mintNFT{value: 0.01 ether}(address(1), "https://pixabay.com/photos/new-zealand-volcano-crater-3018634/"); 
-        vm.prank(address(address(1)));
-        volcanoNFT.approve(address(2), itemId);
-        volcanoNFT.transferOwnership(address(2));
-        assertEq(volcanoNFT.owner(), address(2));
+        vm.prank(address(dave));
+        uint256 itemId = volcanoNFT.mintNFT{value: 0.01 ether}(address(alice), "https://pixabay.com/photos/new-zealand-volcano-crater-3018634/"); 
+        vm.prank(address(address(alice)));
+        volcanoNFT.approve(address(bob), itemId);
+        vm.prank(address(dave)); 
+        volcanoNFT.transferOwnership(address(bob));
+        assertEq(volcanoNFT.owner(), address(bob));
     }
 
 }
