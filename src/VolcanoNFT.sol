55ee24e002ec27962869cd3db4f1c1fc43a35c0b
ericbadiere <eric.badiere@coinme.com>
2022-11-05 21:35:41 -0600

    Working, chekcpoint

--- src/VolcanoNFT.sol
+++ src/VolcanoNFT.sol
@@ -1,22 +1,35 @@
 //Contract based on [https://docs.openzeppelin.com/contracts/3.x/erc721](https://docs.openzeppelin.com/contracts/3.x/erc721)
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity >=0.8.10;
 
+import {console} from "forge-std/console.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
+
+error MintPriceNotPaid();
+error MaxSupply();
+
 contract VolcanoNFT is ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
+
     Counters.Counter private _tokenIds;
 
-    constructor() ERC721("MyNFT", "NFT") {}
+    uint256 public constant TOTAL_SUPPLY = 10_000;
+    uint256 public constant MINT_PRICE = 0.01 ether;
+
+    constructor() ERC721("VolcanoNFT", "NFT") {}
 
     function mintNFT(address recipient, string memory tokenURI)
-        public onlyOwner
+        public payable onlyOwner
         returns (uint256)
     {
+        console.log("%s: %d", "msg.value", msg.value);
+        if (msg.value != MINT_PRICE) {
+            revert MintPriceNotPaid();
+        }
         _tokenIds.increment();
 
         uint256 newItemId = _tokenIds.current();
--- test/VolcanoNFT.t.sol
+++ test/VolcanoNFT.t.sol
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 
-pragma solidity ^0.8.0;
+pragma solidity >=0.8.10;
 
 import "forge-std/Test.sol";
+import {console} from "forge-std/console.sol";
 import "../src/VolcanoNFT.sol";
 
 import {Utils} from "./utils/Utils.sol";
@@ -14,32 +15,43 @@ contract VolcanoNFTTest is Test {
 
     address internal alice;
     address internal bob;
+    address internal dave;
 
     VolcanoNFT public volcanoNFT;
 
     function setUp()public {
-        utils = new Utils();
-        users = utils.createUsers(2);
-        alice = users[0];
-        vm.label(alice, "Alice");
-        bob = users[1];
-        vm.label(bob, "Bob");
+        // utils = new Utils();
+        // users = utils.createUsers(3);
+        // alice = users[0];
+        // vm.label(alice, "Alice");
+        // bob = users[1];
+        // vm.label(bob, "Bob");
+        // dave = users[2];
 
+        // vm.prank(address(dave));
         volcanoNFT = new VolcanoNFT();
     }
 
     function testMint() public {
-        uint256 itemId = volcanoNFT.mintNFT(alice, "https://pixabay.com/photos/new-zealand-volcano-crater-3018634/");
+
+        // console.log("%s: %d", "Dave", address(dave));
+
+        // emit log_address(volcanoNFT.owner());
+
+        // vm.prank(address(dave));
+
+        uint256 itemId = volcanoNFT.mintNFT{value: 0.01 ether}(address(1), "https://pixabay.com/photos/new-zealand-volcano-crater-3018634/");
         assert(itemId > 0);
-        assertEq(volcanoNFT.balanceOf(alice), 1);
+        assertEq(volcanoNFT.balanceOf(address(1)), 1);
     }
 
     function testTransfer() public {
-        uint256 itemId = volcanoNFT.mintNFT(alice, "https://pixabay.com/photos/new-zealand-volcano-crater-3018634/"); 
-        vm.prank(address(alice));
-        volcanoNFT.approve(bob, itemId);
-        volcanoNFT.transferOwnership(bob);
-        assertEq(volcanoNFT.owner(), bob);
+        // vm.prank(address(dave));
+        uint256 itemId = volcanoNFT.mintNFT{value: 0.01 ether}(address(1), "https://pixabay.com/photos/new-zealand-volcano-crater-3018634/"); 
+        vm.prank(address(address(1)));
+        volcanoNFT.approve(address(2), itemId);
+        volcanoNFT.transferOwnership(address(2));
+        assertEq(volcanoNFT.owner(), address(2));
     }
 
 }
